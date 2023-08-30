9fe3eb9dbc363f5bb310d7aca38d3a79ef867a86
lucasnikerj7 <lucas.alonso99@gmail.com>
2023-08-30 12:31:20 -0300

    projeto de cadastro finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+        Scanner sc = new Scanner(System.in);
+
+
+    System.out.println("Por favor, digite o número da Agência!");
+    int numeroAgencia = sc.nextInt();
+
+    System.out.println("Por favor, digite a Agência!");
+    String agencia = sc.next();
+
+    System.out.println("Por favor, digite o nome do Cliente");
+    String cliente = sc.next();
+    sc.nextLine();
+    
+    System.out.println("Saldo: ");
+    double saldo = sc.nextDouble();
+    
+
+    System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", cliente, agencia, numeroAgencia, saldo );
+
+
+    sc.close();
+ }}
